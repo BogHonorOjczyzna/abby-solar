97cd213c0db133428c39ddf964fa599c5f64f837
JakeNexLevel <86127330+JakeNexLevel@users.noreply.github.com>
2023-12-19 14:36:53 -0800

    solar landing page

---
+++ main.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+  $("#q1-b1").click(function () {
+    // $("#Create").toggle()
+    $("#question-1").hide()
+    $("#question-2").css("display", "flex")
+  })
+  $("#q2-b1").click(function () {
+    // $("#Create").toggle()
+    $("#question-2").hide()
+    $("#question-3").css("display", "flex")
+  })
+  $("#q3-b1").click(function () {
+    // $("#Create").toggle()
+    $("#question-3").hide()
+    $("#question-4").css("display", "flex")
+  })
+  $("#q4-b1").click(function () {
+    // $("#Create").toggle()
+    $("#question-4").hide()
+    $("#question-5").css("display", "flex")
+  })
+})
