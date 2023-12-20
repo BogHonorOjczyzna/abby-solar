a94f348c8a0740b9068d58f8f31444332762b36d
Jake Slemboski <jakeslem@gmail.com>
2023-12-19 18:00:22 -0800

    added additional questions

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 $(document).ready(function () {
+  //ALL OF OUR CONTINUE FUNCTIONS
   $("#q1-b1").click(function () {
     // $("#Create").toggle()
     $("#question-1").hide()
     $("#question-2").css("display", "flex")
   })
-  $("#q2-b1").click(function () {
+  $("#q2-b2").click(function () {
     // $("#Create").toggle()
     $("#question-2").hide()
     $("#question-3").css("display", "flex")
@@ -18,5 +19,22 @@ $(document).ready(function () {
     // $("#Create").toggle()....
     $("#question-4").hide()
     $("#question-5").css("display", "flex")
+  })
+  $("#q5-b1").click(function () {
+    // $("#Create").toggle()....
+    $("#question-5").hide()
+    $("#question-6").css("display", "flex")
+  })
+
+  //ALL OF OUR NO-QUALIFY FUNCTIONS
+  $("#q1-b2").click(function () {
+    // $("#Create").toggle()
+    $("#question-1").hide()
+    $("#no-qualify").css("display", "flex")
+  })
+  $("#q2-b1").click(function () {
+    // $("#Create").toggle()
+    $("#question-2").hide()
+    $("#no-qualify").css("display", "flex")
   })
 })
