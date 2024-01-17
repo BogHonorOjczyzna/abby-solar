7a0034f94e70749c18151fe49e8a8c187e57db05
Jake Slemboski <jakeslem@gmail.com>
2024-01-16 19:23:44 -0800

    added data validation

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+var data = {}
+
 $(document).ready(function () {
   //ALL OF OUR CONTINUE FUNCTIONS
   $("#q1-b1").click(function () {
@@ -13,26 +15,49 @@ $(document).ready(function () {
   })
   $("#q3-b1").click(function () {
     // $("#Create").toggle()
+    data.avgBill = $("select#bill-select option:selected").val()
+    if (data.avgBill == "unselected") {
+      $("#q-3-alert-1").css("display", "block")
+      return
+    }
     $("#question-3").hide()
     $("#question-4").css("display", "flex")
   })
   $("#q4-b1").click(function () {
     // $("#Create").toggle()....
+    data.consultType = "In Person"
     $("#question-4").hide()
     $("#question-5").css("display", "flex")
   })
   $("#q4-b2").click(function () {
     // $("#Create").toggle()....
+    data.consultType = "Virtual"
     $("#question-4").hide()
     $("#question-5").css("display", "flex")
   })
   $("#q5-b1").click(function () {
     // $("#Create").toggle()....
+    data.zip = $("#zip").val()
+    data.address = $("#address").val()
+    if (data.zip == "" || data.zip.length != 5 || data.zip.match(/[^$,.\d]/)) {
+      $("#q-5-alert-1").css("display", "block")
+      return
+    }
     $("#question-5").hide()
     $("#question-6").css("display", "flex")
   })
   $("#q6-b1").click(function () {
     // $("#Create").toggle()....
+    data.fname = $("#fname").val()
+    data.lname = $("#lname").val()
+    data.email = $("#email").val()
+    data.phone = $("#phone").val()
+    if (data.fname == "" || data.lname == "" || data.email == "" || data.phone == "" || data.phone.length != 14 || !(data.email.includes("@") && data.email.includes("."))) {
+      $("#q-6-alert-1").css("display", "block")
+      return
+    }
+    // alert(JSON.stringify(data))
+    doSubmitData(data)
     $("#question-6").hide()
     $("#main-cta").hide()
     $("#success").css("display", "flex")
@@ -52,3 +77,22 @@ $(document).ready(function () {
     $("#no-qualify").css("display", "flex")
   })
 })
+
+function doSubmitData(dataThing) {
+  $.ajax({
+    type: "POST",
+    url: "http://ogre.adc4gis.com/",
+    data: { json: JSON.stringify(dataThing) },
+    success: success
+  })
+}
+
+//FIELD MASKING
+$(document).ready(function () {
+  // Mask which specifies options
+  $("#phone").inputmask({ mask: "(999)-999-9999" })
+})
+
+//VERIFY PHONE NUMBER
+
+//ADDRESS AUTOCOMPLETE
